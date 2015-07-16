1d4bb7b1599dc62eec80d3bca4dd7ad4b7ced1d4
Zavier Boyd <zavier@boyd.org.nz>
2015-07-16 21:21:59 +1200

    change file names delete old heat simulation
    Signed-off-by: Zavier Boyd <zavier@boyd.org.nz>

--- heat_simulation.py
+++ heat_simulation.py
@@ -1,57 +1,87 @@
-#v1.0 
+#v2.0 
 from __future__ import division
-#init room temperatures
-room1 = 100.0
-room2 = 25.0
-room3 = 0.0
+#init material temperatures K
+wall_Temp = 18.0
+inside_Temp = 20.0
+window_Temp = 10.0
+outside_Temp = 7.0
 
-#init variables
-change1_2 = 0
-change2_3 = 0
-changeT = 0
-t=0
+#init thermal masses J/K
+wall_mass = 278666.0 + (2 / 3)
+inside_mass = 45600.0
+window_mass = 47040.0
+outside_mass = 10.0**9
 
-#init things that can't change
-uValue1_2 = 1/10.0
-uValue2_3 = 6.0
-dt=1
-area = ((0.3*4)+0.02)*((0.3*12)+0.2) #m^2
+#init interfaces W/K
+wall_inside_conduction = 269.2656
+inside_window_conduction = 40.65
+window_outside_conduction = 90.90909091
 
-b2 = uValue2_3 * area
-c2 = b2 * dt
+#init joules
+wall_Joules = wall_Temp * wall_mass
+inside_Joules = inside_Temp * inside_mass
+window_Joules = window_Temp * window_mass
+outside_Joules = outside_Temp * outside_mass
+#init time
+time = 0.0
+dt = 60
 
-print area
-heatCapacity2 = 324266+(2.0/3.0)
-heatCapacity3 = 1
-
+walltemps = []
+insidetemps = []
 #simulation
-while room2 > 10:
-#	#calculate temprature of room1 to room2
-#	difference1_2 = room1-room2
-#	a1 = uValue1_2 * difference1_2
-#	b1 = a1 * area
-#	c1 = b1 * dt
-#	change1_2 = c1 / heatCapacity2
-	
-	#calculate temprature of room2 to room3
-	difference2_3 = room3-room2
-	a2 = c2 * difference2_3
-	change2_3 = a2 / heatCapacity2
-	
-	#calculate total temprature differince
-	#changeT = change1_2 - change2_3
-	changeT = change2_3
-	
-	#apply temprature differince
-	room2 += changeT
-	
-	#add delta time to time
-	t += dt
-	
-	#print tempratures
-	#print room1,"room1 Kelvin",
-	print room2,"room2 Kelvin",
-	print room3,"room3 Kelvin"
-	
-#print total time of simulation
-print t
+for i in range(60):
+    # calculate energy flow part 1 Watts
+    wall_to_inside_Watts = wall_inside_conduction * wall_Temp
+    inside_to_wall_Watts = wall_inside_conduction * inside_Temp
+    inside_to_window_Watts = inside_window_conduction * inside_Temp
+    window_to_inside_Watts = inside_window_conduction * window_Temp
+    window_to_outside_Watts = window_outside_conduction * window_Temp
+    outside_to_window_Watts = window_outside_conduction * outside_Temp
+    ##################################################################
+    # calculate energy flow part 2 Joules
+    wall_to_inside_Joules = wall_to_inside_Watts * dt
+    inside_to_wall_Joules = inside_to_wall_Watts * dt
+    inside_to_window_Joules = inside_to_window_Watts * dt
+    window_to_inside_Joules = window_to_inside_Watts * dt
+    window_to_outside_Joules = window_to_outside_Watts * dt
+    outside_to_window_Joules = outside_to_window_Watts * dt
+    #######################################################
+    # calculate joules in each mass
+    wall_Joules += inside_to_wall_Joules - wall_to_inside_Joules
+    inside_Joules += wall_to_inside_Joules + window_to_inside_Joules - inside_to_wall_Joules - inside_to_window_Joules
+    window_Joules += inside_to_window_Joules + outside_to_window_Joules - window_to_inside_Joules - window_to_outside_Joules
+    outside_Joules += window_to_outside_Joules - outside_to_window_Joules
+    ########################################################################################################################
+    # calculate new temperatures
+    wall_temp_test = wall_Temp * (1 - wall_inside_conduction * dt * (1 / wall_mass)) + \
+                     inside_Temp *   (wall_inside_conduction * dt * (1 / wall_mass))
+#    wall_temp_test = (wall_Temp * wall_mass + wall_inside_conduction * inside_Temp * dt - wall_inside_conduction * wall_Temp * dt) * (1 / wall_mass)
+#    wall_temp_test = (wall_Joules + wall_inside_conduction * inside_Temp * dt - wall_inside_conduction * wall_Temp * dt) * (1 / wall_mass)
+        #wall_Temp * (wall_inside_conduction * dt) + inside_Temp * (-wall_inside_conduction * dt)
+    wall_Temp = wall_Joules * (1 / wall_mass)
+    print wall_temp_test, wall_Temp
+    inside_Temp = inside_Joules * (1 / inside_mass)
+    window_Temp = window_Joules * (1 / window_mass)
+    outside_Temp = outside_Joules * (1 / outside_mass)
+    ##################################################
+    walltemps.append(wall_Temp)
+    insidetemps.append(inside_Temp)
+    print "{wall_Temp:4.2f} K Wall".format(wall_Temp=wall_Temp),
+    print "{inside_Temp:4.2f} K Inside".format(inside_Temp=inside_Temp),
+    print "{window_Temp:4.2f} K Window".format(window_Temp=window_Temp),
+    print "{outside_Temp:4.2f} K Outside".format(outside_Temp=outside_Temp)
+    time += dt
+
+
+print time,"Time t"
+#print walltemps
+
+# [a b c] [x]   [ax+by+cz]
+# [d e f] [y] = [dx+ey+fz]
+# [g h i] [z]   [gx+hy+iz]
+#
+# [a b] [x]   [ax+by]   [x']
+# [d e] [y] = [dx+ey] = [y']
+#
+# [wall_inside_conduction,0] [wall_Temp]    = [wall_to_inside_Watts] ... [new_wall_Temp]
+# [0,wall_inside_conduction] [inside_Temp]  = [inside_to_wall_Watts] ... [new_inside_Temp]
